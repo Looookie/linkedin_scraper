d7bb3ce1fbf73762bd26ea2b74f70092872cb8f0
Luis <loooo.wang@gmail.com>
2021-11-22 17:40:38 +0800

    chore: output detail info

--- samples/scrape_person.py
+++ samples/scrape_person.py
@@ -7,3 +7,13 @@ email = os.getenv("LINKEDIN_USER")
 password = os.getenv("LINKEDIN_PASSWORD")
 actions.login(driver, email, password) # if email and password isnt given, it'll prompt in terminal
 person = Person("https://www.linkedin.com/in/andre-iguodala-65b48ab5", driver=driver)
+print("Person: " + person.name)
+print("Person About: ")
+print(person.about)
+print("Person Experiences: ")
+for experience in person.experiences:
+    print(f"--: {experience.institution_name} - {experience.position_title}")
+    print(
+        f"from {experience.from_date} to {experience.to_date} ({experience.duration})")
+    print(
+        f"{experience.description}")
